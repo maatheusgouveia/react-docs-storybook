0fdb75301860c984d627030d4d42a7a9ca1ba527
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2022-10-08 16:18:01 -0300

    lint files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,17 @@
 module.exports = {
-  content: ["./src/**/*.tsx"],
-  theme: {
-    extend: {
-      colors: {
-        brand: {
-          300: '#996dff',
-          500: '#8257e6',
-        }
-      },
-      borderRadius: {
-        md: '4px'
-      }
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('tailwind-scrollbar'),
-  ],
-}
+	content: ["./src/**/*.tsx"],
+	theme: {
+		extend: {
+			colors: {
+				brand: {
+					300: "#996dff",
+					500: "#8257e6",
+				},
+			},
+			borderRadius: {
+				md: "4px",
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar")],
+};
